7801835465109f6d6d3387e314c544ed37b69c0a
BIGEL Thomas <thomas.bigel4@etu.univ-lorraine.fr>
2022-01-11 01:05:19 +0100

    debut lesson 1 tinyraytracer var2

---
+++ build/CMakeFiles/tinyraytracer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinyraytracer.dir/tinyraytracer.cpp.obj"
+  "CMakeFiles/tinyraytracer.dir/tinyraytracer.cpp.obj.d"
+  "libtinyraytracer.dll.a"
+  "tinyraytracer.exe"
+  "tinyraytracer.exe.manifest"
+  "tinyraytracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tinyraytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
